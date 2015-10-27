5d8997afcc5216709f9119c2be57dabf767a8da5
veni <viniciusferreirawk@gmail.com>
2015-10-27 11:39:36 -0200

    fix api maps

--- www/js/services/MapsHandlerService.js
+++ www/js/services/MapsHandlerService.js
@@ -8,30 +8,55 @@ angular.module('MapsHandlerService', [])
 
 
   /* element -> DOM element que será montado o mapa*/
-  function configureFirstTimeMap(element){
+  function setMyFirstPos(){
       
-      getPosition(function(lat,lon){ 
-          var minhaPosicao = new google.maps.LatLng(lat, lon);
-          
-          var mapOptions = {
-            center: minhaPosicao,
-            zoom: 16,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-          };
+      getNewPosition(function(lat,lon){ 
+          if(getMapInstance() != null){
+            map = window.mapApp;
+          }else{
+            map = mountMap(element,lat,lon);
+          }
+
+          addMarker(map,lat,lon);
 
           
-          var map = new google.maps.Map(element, mapOptions);
-          addMarker(map,lat,lon);
+         
       });
 
-      
+         
   }
 
-  function getPosition(callback){
+  function mountMap(element,lat,lon){
+        var minhaPosicao = new google.maps.LatLng(lat, lon);
+          
+        var mapOptions = {
+          center: minhaPosicao,
+          zoom: 16,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+
+        if(window.mapApp == undefined){
+          var map = new google.maps.Map(element, mapOptions);
+          window.mapApp = map;
+        }else{
+          map = window.mapApp;
+        }
+
+        return map;       
+  }
+
+  function setPosition(lat,lng){
+    window.mapLat = lat;
+    window.mapLng = lng;
+  }
+
+  function getPosition(){
+    return new google.maps.LatLng(window.mapLat, window.mapLng);
+  }
+  function getNewPosition(callback){
       navigator.geolocation.getCurrentPosition(function(pos) {
           callback(pos.coords.latitude, pos.coords.longitude);
       }); 
-
       
   }
 
@@ -51,26 +76,54 @@ angular.module('MapsHandlerService', [])
 
   }
 
-  /*
-  google.maps.event.addDomListener(window, 'load', function() {
-                $scope.map = map;
-    });
- 
+  function setMapEventListener(mapDom){
+      element = document.querySelector('#'+mapDom);
+      mapEvent = google.maps.event.addDomListener(window, 'load',setMyFirstPos(element) );
 
-  */
+      return element;
+  }
+
+
+
+  function getMapInstance(){
+      if(typeof(window.mapApp) != "undefined")
+        return window.mapApp;
+      return null;
+
+  }
+
+  function addRadiusFocus(map,lat,lon){
+    
+    area = 10;
+
+    var cityCircle = new google.maps.Circle({
+      strokeColor: '#FF0000',
+      strokeOpacity: 0.8,
+      strokeWeight: 2,
+      fillColor: '#FF0000',
+      fillOpacity: 0.35,
+      map: map,
+      center: {lat: lat, lng: lon},
+      radius: area * 10
+    });
+
+  }
 
   return {
     /*maDom -> string com o id do elemento para ser montado*/
     initMap: function(mapDom) {
-      element = document.querySelector('#'+mapDom);
-      mapEvent = google.maps.event.addDomListener(window, 'load',configureFirstTimeMap(element) );
-
+      setMapEventListener(mapDom);
+      //mountMap(element);
     },
-    
+
+    setMapEventListener: setMapEventListener,
     getPosition : getPosition,
-    addMarker: addMarker
-
-
-    
+    setPosition : setPosition,
+    getNewPosition : getNewPosition,
+    addMarker: addMarker,
+    addRadiusFocus: addRadiusFocus,
+    setMyFirstPos : setMyFirstPos,
+    mountMap: mountMap,
+    getMapInstance: getMapInstance
   };
 });
